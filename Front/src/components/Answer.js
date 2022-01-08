0ce6d96754d1443842d23842873871ddc77db7bc
Santiago Almendra <santiagoalmendrarojas19@gmail.com>
2022-01-08 13:14:18 -0500

    Se mejoro posicion de botenes de votacion

--- Front/src/components/Answer.js
+++ Front/src/components/Answer.js
@@ -3,14 +3,15 @@ import meMusta from "../images/me-gusta.png";
 import noMeGusta from "../images/disgusto.png";
 
 export const Answer = ({ answer, realizarVoto }) => (
-  <aside className="answer">
-    <p>
-      {answer.answer} 
-      <span className='float-end'>
-      <img src={meMusta} alt="" width="50" height="50" onClick={() => realizarVoto(answer, 1)}  />
-      <img src={noMeGusta} alt="" width="50" height="50" onClick={() => realizarVoto(answer, -1)}/>
-      </span>
-      <span className='float-end'>{answer.position} usuarios le han dado me gusta</span>
-    </p>
+  <aside className="answers d-flex">
+
+    <span className='answer'>{answer.answer}</span>
+
+    <span className='parte-votacion'>
+      <div className='posicion'>{answer.position} like</div>
+      <img src={meMusta} alt="" width="35" height="35" onClick={() => realizarVoto(answer, 1)}  />
+      <img src={noMeGusta} alt="" width="35" height="35" onClick={() => realizarVoto(answer, -1)}/>
+    </span>
+
   </aside>
 )
